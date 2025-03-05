77ed6995f97344a054aea092be1a2160489fbc5a
Naveen Raj K <rajnaveen344@gmail.com>
2025-03-06 03:07:31 +0530

    Cleanup

--- src/server.rs
+++ src/server.rs
@@ -22,7 +22,7 @@ pub struct RubyLanguageServer {
 impl RubyLanguageServer {
     pub fn new(client: Client) -> Result<Self> {
         let workspace_manager = WorkspaceManager::new();
-        let analyzer = RubyAnalyzer::new();
+        let analyzer = RubyAnalyzer {};
 
         let parser = RubyParser::new()?;
 
@@ -33,31 +33,6 @@ impl RubyLanguageServer {
             workspace_manager: Arc::new(Mutex::new(workspace_manager)),
             analyzer,
         })
-    }
-
-    // Helper method to get a document either from open documents or from the index
-    fn get_document(&self, uri: &Url) -> Option<RubyDocument> {
-        // First check if the document is open
-        if let Some(doc) = self.document_map.get(uri) {
-            return Some(doc.value().clone());
-        }
-
-        // If not open, check the workspace index
-        // Create a scope for the mutex lock to ensure it's dropped properly
-        let document = {
-            if let Ok(workspace) = self.workspace_manager.lock() {
-                workspace.get_document(uri)
-            } else {
-                None
-            }
-        };
-
-        document
-    }
-
-    // Helper method to parse a document and return the tree
-    fn parse_document(&self, document: &RubyDocument) -> Option<tree_sitter::Tree> {
-        self.parser.parse(document.get_content())
     }
 }
 
