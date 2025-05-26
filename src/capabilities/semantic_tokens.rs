0678041fd2abb668606318b2168181c585d4326d
Naveen Raj K <rajnaveen344@gmail.com>
2025-05-26 20:05:25 +0530

    fix: implement basic semantic tokens response with empty data vector

--- src/capabilities/semantic_tokens.rs
+++ src/capabilities/semantic_tokens.rs
@@ -1,6 +1,6 @@
 use lsp_types::{
-    SemanticTokenModifier, SemanticTokenType, SemanticTokensFullOptions, SemanticTokensLegend,
-    SemanticTokensOptions, SemanticTokensResult, WorkDoneProgressOptions,
+    SemanticTokenModifier, SemanticTokenType, SemanticTokens, SemanticTokensFullOptions,
+    SemanticTokensLegend, SemanticTokensOptions, SemanticTokensResult, WorkDoneProgressOptions,
 };
 
 pub const TOKEN_TYPES: [SemanticTokenType; 23] = [
@@ -54,5 +54,8 @@ pub fn get_semantic_tokens_options() -> SemanticTokensOptions {
 }
 
 pub fn get_semantic_tokens_full(_content: String) -> SemanticTokensResult {
-    todo!()
+    SemanticTokensResult::Tokens(SemanticTokens {
+        result_id: None,
+        data: vec![],
+    })
 }
