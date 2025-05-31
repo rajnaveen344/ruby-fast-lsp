6993592113eb2636eb617e23387086d36c306a52
Naveen Raj K <rajnaveen344@gmail.com>
2025-05-31 11:08:23 +0530

    feat: add return inlay hints for method definitions with begin nodes

--- src/analyzer_prism/visitors/inlay_visitor.rs
+++ src/analyzer_prism/visitors/inlay_visitor.rs
@@ -1,5 +1,5 @@
 use lsp_types::{InlayHint, InlayHintKind, InlayHintLabel, Position};
-use ruby_prism::{visit_def_node, Visit};
+use ruby_prism::Visit;
 
 use crate::types::ruby_document::RubyDocument;
 
@@ -44,7 +44,46 @@ impl<'a> Visit<'a> for InlayVisitor<'a> {
         };
 
         self.inlay_hints.push(hint);
-        visit_def_node(self, node);
+
+        if let Some(body) = node.body() {
+            if let Some(statements) = body.as_statements_node() {
+                if let Some(last_stmt) = statements.body().iter().last() {
+                    let start_offset = last_stmt.location().start_offset();
+                    let position = self.document.offset_to_position(start_offset);
+                    let hint = InlayHint {
+                        position,
+                        label: InlayHintLabel::String(format!("return")),
+                        kind: Some(InlayHintKind::PARAMETER),
+                        text_edits: None,
+                        tooltip: None,
+                        padding_left: None,
+                        padding_right: Some(true),
+                        data: None,
+                    };
+
+                    self.inlay_hints.push(hint);
+                }
+            } else if let Some(begin_node) = body.as_begin_node() {
+                if let Some(statements) = begin_node.statements() {
+                    if let Some(last_stmt) = statements.body().iter().last() {
+                        let start_offset = last_stmt.location().start_offset();
+                        let position = self.document.offset_to_position(start_offset);
+                        let hint = InlayHint {
+                            position,
+                            label: InlayHintLabel::String(format!("return")),
+                            kind: Some(InlayHintKind::PARAMETER),
+                            text_edits: None,
+                            tooltip: None,
+                            padding_left: None,
+                            padding_right: Some(true),
+                            data: None,
+                        };
+
+                        self.inlay_hints.push(hint);
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -67,9 +106,38 @@ mod tests {
 
         let hints = visitor.inlay_hints();
 
-        assert_eq!(hints.len(), 1);
+        assert_eq!(hints.len(), 2);
 
         let hint = &hints[0];
         assert_eq!(hint.position, Position::new(2, 3));
+    }
+
+    #[test]
+    fn test_inlay_visitor_method_definition_with_begin() {
+        let content = "def foo
+  puts 'Hello'
+rescue => e
+  raise e
+end";
+        let uri = Url::parse("file:///test.rb").unwrap();
+        let document = RubyDocument::new(uri, content.to_string(), 1);
+
+        let parse_result = ruby_prism::parse(content.as_bytes());
+        let node = parse_result.node();
+
+        let mut visitor = InlayVisitor::new(&document);
+        visitor.visit(&node);
+
+        let hints = visitor.inlay_hints();
+
+        assert_eq!(hints.len(), 2);
+
+        println!("hints {:?}", hints);
+
+        let hint = &hints[0];
+        assert_eq!(hint.position, Position::new(4, 3));
+
+        let hint = &hints[1];
+        assert_eq!(hint.position, Position::new(1, 2));
     }
 }
