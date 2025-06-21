b1d83c3aaa8a42c6b052f3b4cb823eb148d4df2b
Naveen Raj K <rajnaveen344@gmail.com>
2025-06-21 18:10:49 +0530

    refactor: remove unused inlay hints capability from completion module

--- src/capabilities/completion.rs
+++ src/capabilities/completion.rs
@@ -1,17 +1,7 @@
 use log::info;
-use lsp_types::{
-    CompletionParams, CompletionResponse, InlayHintOptions, InlayHintServerCapabilities,
-    WorkDoneProgressOptions,
-};
+use lsp_types::{CompletionParams, CompletionResponse};
 
 use crate::server::RubyLanguageServer;
-
-pub fn get_inlay_hints_capability() -> InlayHintServerCapabilities {
-    InlayHintServerCapabilities::Options(InlayHintOptions {
-        work_done_progress_options: WorkDoneProgressOptions::default(),
-        resolve_provider: Some(false),
-    })
-}
 
 pub async fn handle_completion(
     _server: &RubyLanguageServer,
